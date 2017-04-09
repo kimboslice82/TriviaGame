7ef56c5e180cd1007e4472b1bb9fd1b99c41e34c
Kimberley Hood <KimberleyHood@kimberleys-mbp.attlocal.net>
2017-04-09 15:02:35 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,32 @@
 window.onload = function() {
-    var seconds = 10;
+    
     var counter=setInterval(timer, 1000); 
     var deadline = 0;
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
-    var unanswered = 0;
+    var correctAnswers = $("#option3", "#option4", "#option8");
+    var incorrectAnswers = [];
+    var unanswered = [];
+    var userGuess = [];
     
-function stop() {
-    //timer.stop(0);
-}
+
+    var timer = setInterval(function() {
+        myTimer()
+    }, 1000);
+        var seconds = 45;
+
+    function myTimer() {
+        document.getElementById("timer").innerHTML = "0:" + seconds--; + "seconds";
+    }
+
+
     
-function timer() {
-    seconds--;
-    document.getElementById("timer").innerHTML="0:" + seconds + " seconds"; 
-    
-  if (seconds === 0) {
-     $("#timer").html("0:0");
-     
-  }
-    
-}
-    
-$("#done").click(function() {
-    $("<div></div>").html("Correct Answers: " + correctAnswers);
-    $("<div></div>").html("Incorrect Answers: " + incorrectAnswers);
-    $("<div></div>").html("Unanswered: " + unanswered);
-})
+/*function gamePlay() {
+   if (userGuess != correctAnswer) {
+        incorrectAnswers++; 
+       }
+    else {
+        correctAnswers++;
+    }
+}*/
     
 
 };
