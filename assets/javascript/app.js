007d7afb52696389d20064dd7e6b95063959006c
Kimberley Hood <KimberleyHood@Kimberleys-MacBook-Pro.local>
2017-04-10 14:09:26 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     var questions = $("#question1", "#question2", "#question3");
-    var correctAnswers = $("#answer3", "#answer4", "#answer8");
+    //var correctAnswers = [];
     var incorrectAnswers = $("#answer1", "#answer2", "#answer5", "#answer6", "#answer7");
     var unanswered = [];
     var userGuess = [];
+   
+    
     
     //the timer starts as soon as the page loads
     var timer = setInterval(function() {
@@ -14,17 +16,27 @@ window.onload = function() {
     function myTimer() {
         document.getElementById("timer").innerHTML = "0:" + seconds--; + "seconds";
     }
-
-
     
-function gamePlay() {
-   if (userGuess != correctAnswer) {
-        incorrectAnswers++; 
-       }
-    else {
-        correctAnswers++;
-    }
+    
+/*$("label" ).on( "click", function() {
+  $( "correct-answers" ).html( $( "label:checked[value=correct]" ).val() + " is checked!" );
+});*/
+
+   
+function setScore() {
+   var score = $("input:radio[value=correct]").length;
+  $("#correct-answers").text("Correct Answers: " + score);
+    
 }
+
+// set score on input change
+$("input").change(setScore);
+
+// set score on initialization
+setScore(); {
+    console.log(setScore);
+}
+
     
 
 };
