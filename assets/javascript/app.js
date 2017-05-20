1758958cdd8943870be2b04f0978c8ede5fcd2ff
Kimberley Hood <KimberleyHood@Kimberleys-MacBook-Pro.local>
2017-05-20 09:32:45 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,118 @@
-window.onload = function() {
-    var questions = $("#question1", "#question2", "#question3");
-    //var correctAnswers = [];
-    var incorrectAnswers = $("#answer1", "#answer2", "#answer5", "#answer6", "#answer7");
-    var unanswered = [];
-    var userGuess = [];
-   
-    
-    
-    //the timer starts as soon as the page loads
-    var timer = setInterval(function() {
-        myTimer()
-    }, 1000);
-        var seconds = 45;
+var panel = $("#quiz-area");
 
-    function myTimer() {
-        document.getElementById("timer").innerHTML = "0:" + seconds--; + "seconds";
+// Question set
+var questions = [{
+  question: "Best team in basketball?",
+  answers: ["Golden State Warriors", "Cleveland Cavaliers", "Houston Rockets"],
+  correctAnswer: "Houston Rockets"
+}, {
+  question: "Best player in NBA?",
+  answers: ["James Harden", "Steph Curry", "Russell Westbrook"],
+  correctAnswer: "James Harden"
+}, {
+  question: "Greatest Basketball City?",
+  answers: ["Cleveland", "H-town", "Miami"],
+  correctAnswer: "H-town"
+}];
+
+// Variable that will hold the setInterval
+var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 30,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      game.done();
     }
+  },
     
     
-/*$("label" ).on( "click", function() {
-  $( "correct-answers" ).html( $( "label:checked[value=correct]" ).val() + " is checked!" );
-});*/
+    start: function() {
 
-   
-function setScore() {
-   var score = $("input:radio[value=correct]").length;
-  $("#correct-answers").text("Correct Answers: " + score);
-    
-}
+    $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
 
-// set score on input change
-$("input").change(setScore);
+    $("#start").remove();
 
-// set score on initialization
-setScore(); {
-    console.log(setScore);
-}
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
 
-    
+    panel.append("<button id='done'>Done</button>");
+  },
 
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#subwrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
 };
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+  game.countdown();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
